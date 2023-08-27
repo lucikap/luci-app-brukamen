f686995ad336e5cd396d7e12579f504dba41ee57
lucikap <133383664+lucikap@users.noreply.github.com>
2023-08-27 14:41:09 +0800

    Update suselogin.lua

--- luci-app-suselogin/files/root/usr/lib/lua/luci/controller/suselogin.lua
+++ luci-app-suselogin/files/root/usr/lib/lua/luci/controller/suselogin.lua
@@ -5,7 +5,7 @@
 module("luci.controller.suselogin", package.seeall)
 
 function index()
-        entry({"admin", "school", "suselogin"},firstchild(), _("川轻化web"), 100).dependent = false
-        entry({"admin", "school", "suselogin", "general"}, cbi("suselogin"), _("配置"), 1)
-        entry({"admin", "school", "suselogin", "log"}, form("suseloginlog"), _("运行日志"), 2)
+        entry({"admin", "services", "suselogin"},firstchild(), _("川轻化web"), 100).dependent = false
+        entry({"admin", "services", "suselogin", "general"}, cbi("suselogin"), _("配置"), 1)
+        entry({"admin", "services", "suselogin", "log"}, form("suseloginlog"), _("运行日志"), 2)
 end
